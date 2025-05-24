09e9fcbcfbf001b86c874cf79b4280a844dc1bae
Rina-ui <marinagracia895@gmail.com>
2025-05-24 11:34:31 +0200

    modification

---
+++ lib/data/utilisateurList.dart
@@ -0,0 +1,44 @@
+import 'package:salleamphi/modal/user.dart';
+
+List<User> listUser = [
+  User(
+      id: 1,
+      nom: 'JOHN',
+      post: 'Manager',
+      numero: 92938764,
+      email: 'john@gmail.com',
+      profil: ''
+  ),
+  User(
+      id: 2,
+      nom: 'JOHN',
+      post: 'Manager',
+      numero: 92938764,
+      email: 'johngmail.com',
+      profil: ''
+  ),
+  User(
+      id: 3,
+      nom: 'JOANA',
+      post: 'Manager',
+      numero: 92938764,
+      email: 'john@gmail.com',
+      profil: ''
+  ),
+  User(
+      id: 4,
+      nom: 'JOHN',
+      post: 'Manager',
+      numero: 92938764,
+      email: 'john@gmail.com',
+      profil: ''
+  ),
+  User(
+      id: 5,
+      nom: 'JOHN',
+      post: 'Manager',
+      numero: 92938764,
+      email: 'john@gmail.com',
+      profil: ''
+  )
+];
---
+++ lib/modal/user.dart
@@ -0,0 +1,16 @@
+class User{
+  int id;
+  String nom;
+  String post;
+  int numero;
+  String email;
+  String profil;
+
+  User({required this.id,
+    required this.nom,
+    required this.post,
+    required this.numero,
+    required this.email,
+    required this.profil});
+
+}
